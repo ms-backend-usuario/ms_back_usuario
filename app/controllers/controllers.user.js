0b3b646432b8dbcae6da70b59744c1b063674155
Carolina <ccastrillon614@gmail.com>
2024-05-09 11:32:43 -0500

    Todo hasta el token

--- app/controllers/controllers.user.js
+++ app/controllers/controllers.user.js
@@ -4,20 +4,21 @@ import bcrypt from "bcrypt";
 import { success, error } from "../message/browser.js" //6 paso, cambiamos este paso del routes.user.js
 import pool from "../config/db.msql.js";
 import { config } from "dotenv";
+import jwt from "jsonwebtoken";
 config();
- 
-export const crearUser = async(req, res)=>{
+
+export const crearUser = async (req, res) => {
     const nombre = req.body.nombre;
     const usuario = req.body.usuario;
     const claveSinCifrar = req.body.clave;
-    
+
     try {
         const hash = await bcrypt.hash(claveSinCifrar, 2);
         const clave = hash;
         const respuesta = await pool.query(`CALL sp_CrearUsuario('${nombre}', '${usuario}', '${clave}'); `);
-        if (respuesta[0].affectedRows == 1){
+        if (respuesta[0].affectedRows == 1) {
             success(req, res, 201, "Usuario creado");
-        }else{
+        } else {
             error(req, res, 400, "No se pudo agregar el nuevo usuario");
         }
     } catch (err) {
@@ -25,7 +26,7 @@ export const crearUser = async(req, res)=>{
     }
 };
 
-export const mostrarUser = async(req, res)=>{
+export const mostrarUser = async (req, res) => {
 
     let id = req.params['id'];
 
@@ -34,22 +35,22 @@ export const mostrarUser = async(req, res)=>{
         success(req, res, 200, respuesta[0]);
     } catch (err) {
         error(req, res, 500, err)
-        
+
     }
 };
 
-export const listarUser = async(req, res)=>{
+export const listarUser = async (req, res) => {
     try {
         const respuesta = await pool.query(`CALL sp_ListarUsuario(); `);
         success(req, res, 200, respuesta[0]);
     } catch (err) {
         error(req, res, 500, err)
-        
+
     }
 
 };
 
-export const modificarUser = async(req, res)=>{
+export const modificarUser = async (req, res) => {
     const id = req.body.id;
     const nombre = req.body.nombre;
     const usuario = req.body.usuario;
@@ -57,9 +58,9 @@ export const modificarUser = async(req, res)=>{
     const clave = claveSinCifrar;
     try {
         const respuesta = await pool.query(`CALL sp_ModificarUsuario(${id}, '${nombre}', '${usuario}', '${clave}'); `);
-        if (respuesta[0].affectedRows == 1){
-            success(req, res, 201, "Usuario Modificado: "+ usuario);
-        }else{
+        if (respuesta[0].affectedRows == 1) {
+            success(req, res, 201, "Usuario Modificado: " + usuario);
+        } else {
             error(req, res, 400, "No se pudo modificar el usuario: " + usuario);
         }
     } catch (err) {
@@ -67,13 +68,13 @@ export const modificarUser = async(req, res)=>{
     }
 };
 
-export const eliminarUser = async(req, res)=>{
+export const eliminarUser = async (req, res) => {
     const id = req.body.id;
     try {
         const respuesta = await pool.query(`CALL sp_EliminarUsuario(${id}) `);
-        if (respuesta[0].affectedRows == 1){
+        if (respuesta[0].affectedRows == 1) {
             success(req, res, 201, "Usuario Eliminado:");
-        }else{
+        } else {
             error(req, res, 400, "No se pudo elimimar el usuario:");
         }
     } catch (err) {
@@ -81,19 +82,35 @@ export const eliminarUser = async(req, res)=>{
     }
 };
 
-export const logueoUser = async(req, res) =>{
-    const {usuario, clave} = req.body;
-    const hash = await bcrypt.hash(clave, 2);
-    try{
+export const logueoUser = async (req, res) => {
+    const { usuario, clave } = req.body;
+    // const hash = await bcrypt.hash(clave, 2);
+    try {
         const respuesta = await pool.query(`CALL sp_BuscarUsuario('${usuario}'); `);
-        if(respuesta[0][0].length==0){
-            const error = new Error("Usuario no existe");
-            res.status(404).json({ error: error.message })
+        if (respuesta[0][0] == 0) {
+            error(req, res, 404, "Usuario no existe");
             return;
         }
-        res.json(respuesta[0]);
-    }catch (error) {
-        res.json(error);
+        const match = await bcrypt.compare(clave, respuesta[0][0][0].CLAVE);
+        if (!match) {
+            error(req, res, 401, "Clave Errada");
+            return;
+        }
+        let payload = {
+            "usuario": usuario,
+            "nombre": respuesta[0][0][0].NOMBRE
+        };
+        let token = jwt.sign(
+            payload,
+            process.env.TOKEN_PRIVATEKEY,
+            {
+                expiresIn: process.env.TOKEN_EXPIRES_IN
+            });
+        success(req, res, 200, { token });
+
+
+    } catch (e) {
+        error(req, res, 500, "Error en el servidor, por favor intente de nuevo mas tarde");
 
     }
 }
--- app/routes/routes.user.js
+++ app/routes/routes.user.js
@@ -1,7 +1,9 @@
 import { Router } from "express"; //1 paso
 import { crearUser, mostrarUser, modificarUser, eliminarUser, listarUser, logueoUser } from "../controllers/controllers.user.js"
+import { verifyToken } from "../middlewares/oauth.js";
 
 const rutaUser = Router(); //2 paso
+
 
 //3 paso, empezar con las rutas -> get sirve para mostrar datos
 rutaUser.get("/user/:id", mostrarUser);
@@ -10,13 +12,13 @@ rutaUser.get("/user/:id", mostrarUser);
 rutaUser.get("/user", listarUser);
 
 //11 paso -> post sirve para guardar o crear datos
-rutaUser.post("/user", crearUser);
+rutaUser.post("/user", verifyToken, crearUser);
 
 // 13 paso -> modificar
-rutaUser.put("/user", modificarUser);
+rutaUser.put("/user", verifyToken, modificarUser);
 
 //14 paso -> para borrar
-rutaUser.delete("/user", eliminarUser);
+rutaUser.delete("/user", verifyToken, eliminarUser);
 
 //para loguearse
 rutaUser.post("/login", logueoUser);
