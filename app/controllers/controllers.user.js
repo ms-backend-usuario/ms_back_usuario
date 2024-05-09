d5c2d66de866a101456300dd15e4193dace8136f
Carolina <ccastrillon614@gmail.com>
2024-05-09 07:44:49 -0500

    Validacion si el usuario existe o no existe

--- app/controllers/controllers.user.js
+++ app/controllers/controllers.user.js
@@ -1,18 +1,36 @@
 //paso 16 creamos la carpeta de controllers y el este archivo controllers.user.js
 //paso 17
+import bcrypt from "bcrypt";
 import { success, error } from "../message/browser.js" //6 paso, cambiamos este paso del routes.user.js
 import pool from "../config/db.msql.js";
+import { config } from "dotenv";
+config();
  
-export const crearUser = (req, res)=>{
-    const dato = req.body;
-    console.log(dato);
-    success(req, res, 201, "Post: Ha ingresado un dato")
+export const crearUser = async(req, res)=>{
+    const nombre = req.body.nombre;
+    const usuario = req.body.usuario;
+    const claveSinCifrar = req.body.clave;
+    
+    try {
+        const hash = await bcrypt.hash(claveSinCifrar, 2);
+        const clave = hash;
+        const respuesta = await pool.query(`CALL sp_CrearUsuario('${nombre}', '${usuario}', '${clave}'); `);
+        if (respuesta[0].affectedRows == 1){
+            success(req, res, 201, "Usuario creado");
+        }else{
+            error(req, res, 400, "No se pudo agregar el nuevo usuario");
+        }
+    } catch (err) {
+        error(req, res, 400, err);
+    }
 };
 
 export const mostrarUser = async(req, res)=>{
+
+    let id = req.params['id'];
+
     try {
-        const respuesta = await pool.query(" CALL sp_MostrarUsuario(1); ")
-        console.log(respuesta);
+        const respuesta = await pool.query(`CALL sp_MostrarUsuario(${id}); `);
         success(req, res, 200, respuesta[0]);
     } catch (err) {
         error(req, res, 500, err)
@@ -20,12 +38,62 @@ export const mostrarUser = async(req, res)=>{
     }
 };
 
-export const modificarUser = (req, res)=>{
-    const dato = req.body;
-    success(req, res, 200, "Put: Ha Modificado un dato")
+export const listarUser = async(req, res)=>{
+    try {
+        const respuesta = await pool.query(`CALL sp_ListarUsuario(); `);
+        success(req, res, 200, respuesta[0]);
+    } catch (err) {
+        error(req, res, 500, err)
+        
+    }
+
 };
 
-export const eliminarUser = (req, res)=>{
-    const dato = req.body;
-    success(req, res, 200, "Delete: Ha Eliminado un dato")
+export const modificarUser = async(req, res)=>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const usuario = req.body.usuario;
+    const claveSinCifrar = req.body.clave;
+    const clave = claveSinCifrar;
+    try {
+        const respuesta = await pool.query(`CALL sp_ModificarUsuario(${id}, '${nombre}', '${usuario}', '${clave}'); `);
+        if (respuesta[0].affectedRows == 1){
+            success(req, res, 201, "Usuario Modificado: "+ usuario);
+        }else{
+            error(req, res, 400, "No se pudo modificar el usuario: " + usuario);
+        }
+    } catch (err) {
+        error(req, res, 400, err);
+    }
 };
+
+export const eliminarUser = async(req, res)=>{
+    const id = req.body.id;
+    try {
+        const respuesta = await pool.query(`CALL sp_EliminarUsuario(${id}) `);
+        if (respuesta[0].affectedRows == 1){
+            success(req, res, 201, "Usuario Eliminado:");
+        }else{
+            error(req, res, 400, "No se pudo elimimar el usuario:");
+        }
+    } catch (err) {
+        error(req, res, 400, err);
+    }
+};
+
+export const logueoUser = async(req, res) =>{
+    const {usuario, clave} = req.body;
+    const hash = await bcrypt.hash(clave, 2);
+    try{
+        const respuesta = await pool.query(`CALL sp_BuscarUsuario('${usuario}'); `);
+        if(respuesta[0][0].length==0){
+            const error = new Error("Usuario no existe");
+            res.status(404).json({ error: error.message })
+            return;
+        }
+        res.json(respuesta[0]);
+    }catch (error) {
+        res.json(error);
+
+    }
+}
--- app/routes/routes.user.js
+++ app/routes/routes.user.js
@@ -1,10 +1,13 @@
 import { Router } from "express"; //1 paso
-import { crearUser, mostrarUser, modificarUser, eliminarUser } from "../controllers/controllers.user.js"
+import { crearUser, mostrarUser, modificarUser, eliminarUser, listarUser, logueoUser } from "../controllers/controllers.user.js"
 
 const rutaUser = Router(); //2 paso
 
 //3 paso, empezar con las rutas -> get sirve para mostrar datos
-rutaUser.get("/user", mostrarUser);
+rutaUser.get("/user/:id", mostrarUser);
+
+//
+rutaUser.get("/user", listarUser);
 
 //11 paso -> post sirve para guardar o crear datos
 rutaUser.post("/user", crearUser);
@@ -15,7 +18,8 @@ rutaUser.put("/user", modificarUser);
 //14 paso -> para borrar
 rutaUser.delete("/user", eliminarUser);
 
-//4 paso en browser.js//
+//para loguearse
+rutaUser.post("/login", logueoUser);
 
 export default rutaUser; //8 paso
 //9,10 paso en app.js
